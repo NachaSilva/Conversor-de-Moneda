7d3c029146117993b9b3777346a54c00b49f9397
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2022-12-18 16:30:31 -0300

    Finalizando conversor de monedas

--- src/ConversorMonedas/Principal.java
+++ src/ConversorMonedas/Principal.java
@@ -1,27 +1,36 @@
 package ConversorMonedas;
 
-
 import javax.swing.*;
 
 public class Principal {
-	
+
 	static Function monedas = new Function();
-	
+
 	public static void main(String[] args) {
-		String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menu", JOptionPane.QUESTION_MESSAGE, null, new Object [] {"Conversor de Moneda", "Conversor de Temperatura"},"Seleccion")).toString();
-		
-		switch(opciones) {
-		
-		
-		
-		case "Conversor de Moneda": {
-			String input = JOptionPane.showInputDialog("Ingrese un valor para convertir");
-			double ValorRecibido = Double.parseDouble(input);			
-			monedas.ConvertirMonedas(ValorRecibido);
-		}
-		
-		
-		
+		boolean next = true;
+		while (next) {
+			String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", "Menu",
+					JOptionPane.QUESTION_MESSAGE, null,
+					new Object[] { "Conversor de Moneda", "Conversor de Temperatura" }, "Seleccion")).toString();
+
+			switch (opciones) {
+
+			case "Conversor de Moneda": {
+				String input = JOptionPane.showInputDialog("Ingrese un valor para convertir");
+				double ValorRecibido = Double.parseDouble(input);
+				monedas.ConvertirMonedas(ValorRecibido);
+			}
+
+				int Seleccion = JOptionPane.showConfirmDialog(null, "¿Deseas realizar otra operación?");
+				if (JOptionPane.OK_OPTION != Seleccion) {
+					JOptionPane.showMessageDialog(null, "Programa Terminado");
+					next = false;
+
+				
+			}
+			case "Conversor de Temperatura"
+
 		}
 	}
+}
 }
